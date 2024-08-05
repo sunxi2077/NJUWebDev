6d3d8ab47b3b4c1af9d6b6c53f220b5b38e1e802
xySpider <sunxinyuan2077@163.com>
2024-08-05 21:38:22 +0800

    after init

---
+++ webdev/backend/signboard/src/config/config.default.ts
@@ -0,0 +1,13 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1721655911737_3824',
+  koa: {
+    port: 7001,
+  },
+  webSocket:{
+    port: 3000
+  }
+  
+} as MidwayConfig;
---
+++ webdev/frontend/signboard/src/LogInPage.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './LogInPAge.css'
+
+function App() {
+    const [count, setCount] = useState(0)
+    return (
+        <>
+            <div>
+                <a href="https://www.baidu.com" target="_blank">
+                    <img src={} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://www.github.com" target="_blank">
+                    <img src={} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>Welcome to Your Board</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                {/* <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+        </p> */}
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    )
+}
+
+export default App
